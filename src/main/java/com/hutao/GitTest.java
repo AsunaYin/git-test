648a7bb59e93d1390ba39c377c9d9844e3d61e3d
YukiAsuna <52905159+AsunaYin@users.noreply.github.com>
2022-02-09 23:08:11 +0800

    Update GitTest.java
    pull commit

--- src/main/java/com/hutao/GitTest.java
+++ src/main/java/com/hutao/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
 		System.out.println("Hello!Master!");
 		System.out.println("Hello!HotFix!");
 		System.out.println("push test!");
+		System.out.println("pull test!");
 	}
 }
