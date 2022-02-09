c46b4b7fac7b4819b6e2828bb1616aded921be75
zzk <1067133459@qq.com>
2022-02-09 17:21:37 +0800

    first commit

---
+++ src/main/java/com/hutao/GitTest.java
@@ -0,0 +1,14 @@
+package com.hutao;
+
+/**
+ * @author HUTAO
+ * @Description
+ * @date 2022/2/9 17:20
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+		System.out.println("Hello!Git!");
+		System.out.println("Hello!");
+	}
+}
